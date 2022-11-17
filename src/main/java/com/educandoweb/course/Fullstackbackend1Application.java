9b857d789326201bad961ed10a61aa3fcdb50b92
Bruno M Belo <bmbelo87@gmail.com>
2022-11-17 14:24:19 -0300

    Projeto criado

---
+++ src/main/java/com/educandoweb/course/Fullstackbackend1Application.java
@@ -0,0 +1,13 @@
+package com.educandoweb.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fullstackbackend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fullstackbackend1Application.class, args);
+	}
+
+}
